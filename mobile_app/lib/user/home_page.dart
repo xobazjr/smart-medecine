7c9648b08e0e12a8e1cf8c19a9d164734cdfb313
xobazjr <MR.TeerapatKotanartChannel@Gmail.COM>
2026-02-22 13:51:46 +0700

    Update fetch data from the internet
    ✅ Commend some import don't use
    ✅ Add or import http to home_page
    ✅ Add Required to fetch data from the internet, Android, IOS

--- mobile_app/lib/user/home_page.dart
+++ mobile_app/lib/user/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
