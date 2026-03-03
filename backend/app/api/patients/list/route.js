e7a3690c3f9b588bd0f47b3a7d8bbef29d522065
Phongsiri Loedphongthai <p.loedphongthai@email.kmutnb.ac.th>
2026-03-03 11:07:24 +0700

    user_id in list

--- backend/app/api/patients/list/route.js
+++ backend/app/api/patients/list/route.js
@@ -8,7 +8,7 @@ export async function GET(req) {
         const { searchParams } = new URL(req.url);
         const caretaker = searchParams.get('caretaker_name');
         const find_caretaker = await sql`SELECT u.user_id FROM users u WHERE u.username = ${caretaker}`;
-        const find_patient_from_caretaker = await sql`SELECT u.username FROM users u WHERE u.caretaker_id = ${find_caretaker[0].user_id}`;
+        const find_patient_from_caretaker = await sql`SELECT u.username, u.user_id FROM users u WHERE u.caretaker_id = ${find_caretaker[0].user_id}`;
         return NextResponse.json(find_patient_from_caretaker);
 
     } catch (e) {
