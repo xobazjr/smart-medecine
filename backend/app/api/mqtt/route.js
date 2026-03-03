f1808993ebb1d8c2ebde7bdfddaa19c546a6bc8c
Phongsiri Loedphongthai <p.loedphongthai@email.kmutnb.ac.th>
2026-03-03 20:47:11 +0700

    [backend] fix api in mqtt status

--- backend/app/api/mqtt/route.js
+++ backend/app/api/mqtt/route.js
@@ -6,13 +6,13 @@ export async function POST(req) {
         const body = await req.json();
 
         const { msg, time } = body;
-
-        if (msg !== "I'm alive") {
-            return NextResponse.json(
-                { status: 400, error: "Bad request" },
-                { status: 400 }
-            );
-        }
+        //
+        // if (msg !== "I'm alive") {
+        //     return NextResponse.json(
+        //         { status: 400, error: "Bad request" },
+        //         { status: 400 }
+        //     );
+        // }
         await sql`UPDATE board_status SET timestamp = ${time} WHERE name = 'esp8266_xbjr'`;
         return NextResponse.json({ status: 200, success: true }, { status: 200 } )
     } catch (e) {
