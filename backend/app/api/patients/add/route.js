7e730de8e846ba49dc26a07844082564c42d305b
Phongsiri Loedphongthai <p.loedphongthai@email.kmutnb.ac.th>
2026-03-04 09:55:34 +0700

    fix api add patient ด่วน

--- backend/app/api/patients/add/route.js
+++ backend/app/api/patients/add/route.js
@@ -13,41 +13,33 @@ export async function POST(req) {
         if (!username || !password || !caretaker_name ||
             username === "" || password === "" || caretaker_name === "") {
             return NextResponse.json(
-                { error: "Username, password, or caretaker name is null", status: 204 },
-                { status: 204 }
-            )
+                {error: "Username, password, or caretaker name is null"},
+                {status: 400}
+            );
         }
 
-        const caretaker_id = await sql`select user_id from users where username = ${caretaker_name}`;
+        const caretaker_id = await sql`select user_id
+                                       from users
+                                       where username = ${caretaker_name}`;
+
+        if (caretaker_id.length === 0) {
+            return NextResponse.json({error: "Caretaker not found"}, {status: 404});
+        }
+
         const saltRounds = 10;
         const password_hash = await bcrypt.hash(password, saltRounds);
 
         const status = await sql`
             INSERT INTO users (
-                username,
-                password_hash,
-                tel,
-                               webhook_url_discord,
-                               webhook_url_line,
-                role,
-                caretaker_id,
-                morning_time,
-                noon_time,
-                evening_time,
-                bedtime_time
-            ) VALUES (
-                 ${username},
-                 ${password_hash},
-                 ${tel || ""},
-                      "",
-                      "",
-                 'elderly',
-                 ${caretaker_id[0].user_id},
-                 ${morning_time ?? "8:00:00"},
-                 ${noon_time ?? "12:00:00"},
-                 ${evening_time ?? "18:00:00"},
-                 ${bedtime_time ?? "22:00:00"}     
-            ) RETURNING user_id, username, tel;
+                username, password_hash, tel, webhook_url_discord, webhook_url_line,
+                role, caretaker_id, morning_time, noon_time, evening_time, bedtime_time
+            )
+            VALUES (
+                       ${username}, ${password_hash}, ${tel || ''}, '', '',
+                       'elderly', ${caretaker_id[0].user_id},
+                       ${morning_time ?? '08:00:00'}, ${noon_time ?? '12:00:00'},
+                       ${evening_time ?? '18:00:00'}, ${bedtime_time ?? '22:00:00'}
+                   ) RETURNING user_id, username, tel;
         `;
 
         return NextResponse.json(status);
